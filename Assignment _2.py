563cc1e9c31f5e6bcb79c743ae9eb77397f00775
Ravi Teja Bejugama <148865056+rb23abm@users.noreply.github.com>
2023-12-13 12:37:30 +0000

    Latest Update

--- Assignment _2.py
+++ Assignment _2.py
@@ -1,261 +1,190 @@
+
+#importing required libraries
 import pandas as pd
-import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#setting views for dataframe
-pd.options.display.max_rows = 40
-pd.options.display.max_columns = 40
 
 
-def load_dataframe(filename):
+def load_dataframe_transpose(filename):
     '''
-    Function to load the dataframe and manipulate the country and rows
-    features and return two datframes(Raw Dataframe and Cleaned Dataframe).
+    A function to read the dataframe and transpose the country and rows features and return two datframes.
     '''
-    df_year_col = pd.read_excel(filename,engine="openpyxl")
-    df_temp = pd.melt(
-        df_year_col,
-        id_vars=[
-            'Country Name',
-            'Country Code',
-            'Indicator Name',
-            'Indicator Code'],
-        var_name='Year',
-        value_name='Value')
-    df_country_col = df_temp.pivot_table(
-        index=['Year', 'Country Code', 'Indicator Name', 'Indicator Code'],
-        columns='Country Name',
-        values='Value').reset_index()
-    df_country_col = df_country_col.drop_duplicates().reset_index()
-    return df_year_col,df_country_col
+    df_year_column = pd.read_excel(filename,engine="openpyxl")
+    df_t = pd.melt(df_year_column, id_vars=['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code'],
+                    var_name='Year', value_name='Value')
+    df_country_column = df_t.pivot_table(index=['Year', 'Country Code', 'Indicator Name', 'Indicator Code'], columns='Country Name', values='Value').reset_index()
+    df_country_column = df_country_column.drop_duplicates().reset_index()
+    return df_year_column,df_country_column
 
 
-def years_to_analyze_data(data,year1,year2,frequency = 5):
+
+df_year,df_country = load_dataframe_transpose('world_bank_climate.xlsx')
+
+
+def year_data_to_analyze(data,year_start,year_end,frequency_year):
+    data_sample = data.copy()
+    years_needed=[i for i in range(year_start,year_end,frequency_year)]
+    req_col=['Country Name','Indicator Name']
+    req_col.extend(years_needed)
+    data_sample =  data_sample[req_col]
+    data_sample = data_sample.dropna(axis=0, how="any") #removing all the null value rows
+    return data_sample
+
+#gathering the year data from 1980 to 2020 with 5 interval gap
+df_year_col = year_data_to_analyze(df_year,1980,2020,5)
+
+#taking 8 countires to analyze based on the feature occurances
+countries_to_analyze = df_year_col['Country Name'].value_counts().index.tolist()[1:8]
+
+
+
+print(df_year_col.describe())
+
+
+def filter_field_data(data,feature_to_check,value_list_elements):
     '''
-    function retrieve data from start year to end year based on interval.
+    function to filter dataframe based on feature value.
     '''
-    data_temp = data.copy()
-    years_to_analyze=[i for i in range(year1,year2,frequency)]
-    col_to_keep=['Country Name','Indicator Name']
-    col_to_keep.extend(years_to_analyze)
-    data_temp =  data_temp[col_to_keep]
-    data_temp = data_temp.dropna(axis=0, how="any")
-    return data_temp
+    data_sample = data.copy()
+    data_req= data_sample[data_sample[feature_to_check].isin(value_list_elements)].reset_index(drop=True)
+    return data_req
 
 
-def data_specific_field(data,feature,value_list):
-    '''
-    function to filter dataframe based on values of column.
-    '''
-    data_temp = data.copy()
-    data_field = data_temp[
-        data_temp[feature].isin(value_list)].reset_index(drop=True)
-    return data_field
+
+df_year_col_co2int = filter_field_data(df_year_col,'Indicator Name',['CO2 intensity (kg per kg of oil equivalent energy use)'])
 
 
-def bar_plot_country_year(data,indicator):
-    # Filter numeric columns for plotting
-    df = data.copy()
-    df.set_index('Country Name', inplace=True)
-    numeric_columns = df.columns[df.dtypes == 'float64']
-    df_numeric = df[numeric_columns]
+print(df_year_col_co2int.describe())
 
 
-    # Plotting
+df_year_col_co2_countries  = filter_field_data(df_year_col_co2int,'Country Name',countries_to_analyze)
+
+print(df_year_col_co2_countries.describe())
+
+
+def bar_plot_containing_country_xlabel(data,indicator_variable):
+    df_sample = data.copy()
+    df_sample.set_index('Country Name', inplace=True)
+    numeric_columns_to_keep = df_sample.columns[df_sample.dtypes == 'float64']
+    df_numeric_sample = df_sample[numeric_columns_to_keep]
     plt.figure(figsize=(50, 50))
-    df_numeric.plot(kind='bar')
-    plt.title(indicator)
-    plt.xlabel('Country Name')
-    plt.ylabel('Urban Population (% of Total Population)')
-    plt.legend(title='Year', bbox_to_anchor=(1.05, 1), loc='upper left')
+    df_numeric_sample.plot(kind='bar')
+    plt.title(indicator_variable)
+    plt.xlabel('Country Name')    
+    plt.legend(title='Year', bbox_to_anchor=(1.10, 1), loc='upper left')
     plt.show()
 
 
-def get_data_indicator(data):
-    """
-    function to give indicator names as columns for
-    respective dataframe country
-    """
-    df=data.copy()
-    df_melted = df.melt(
-        id_vars='Indicator Name',
-        var_name='Year', value_name='Value')
-    df_pivoted = df_melted.pivot(
-        index='Year', columns='Indicator Name', values='Value')
-    df_pivoted.reset_index(inplace=True)
-    df_pivoted = df_pivoted.apply(pd.to_numeric, errors='coerce')
-    del df_pivoted['Year']
-    df_pivoted = df_pivoted.rename_axis(None, axis=1)
-    return df_pivoted
+bar_plot_containing_country_xlabel(df_year_col_co2_countries,'CO2 intensity (kg per kg of oil equivalent energy use)')
 
 
-def time_series_plot(data,indicator):
+df_year_col_mort = filter_field_data(df_year_col,'Indicator Name',['Mortality rate, under-5 (per 1,000 live births)'])
+df_year_col_mort  = filter_field_data(df_year_col_mort,'Country Name',countries_to_analyze)
+
+
+print(df_year_col_mort.describe())
+
+
+bar_plot_containing_country_xlabel(df_year_col_mort,'Mortality rate, under-5 (per 1,000 live births)')
+
+
+
+#taking finland country data to analyze
+df_year_finland = filter_field_data(df_year_col,'Country Name',['Finland'])
+
+
+def filter_indicator_data(data):
+    df_s=data.copy()
+    # Melt the DataFrame
+    df_melted_sample = df_s.melt(id_vars='Indicator Name', var_name='Year', value_name='Value')
+
+    # Pivot the DataFrame
+    df_pivoted_sample = df_melted_sample.pivot(index='Year', columns='Indicator Name', values='Value')
+
+    # Reset index
+    df_pivoted_sample.reset_index(inplace=True)
+    df_pivoted_sample = df_pivoted_sample.apply(pd.to_numeric, errors='coerce')
+    del df_pivoted_sample['Year']
+    df_pivoted_sample = df_pivoted_sample.rename_axis(None, axis=1)
+    return df_pivoted_sample
+
+
+
+data_heat_map_finland = filter_indicator_data(df_year_finland)
+
+
+
+data_heat_map_finland_map = data_heat_map_finland[["Urban population growth (annual %)","Mortality rate, under-5 (per 1,000 live births)","CO2 intensity (kg per kg of oil equivalent energy use)","Energy use (kg of oil equivalent per capita)","Electricity production from coal sources (% of total)","School enrollment, primary and secondary (gross), gender parity index (GPI)"]]
+
+
+print(data_heat_map_finland_map.corr())
+
+
+sns.heatmap(data_heat_map_finland_map.corr(), annot=True, cmap='YlGnBu', linewidths=.5, fmt='.3g')
+
+
+df_year_col_energy= filter_field_data(df_year_col,'Indicator Name',['Energy use (kg of oil equivalent per capita)'])
+df_year_col_energy  = filter_field_data(df_year_col_energy,'Country Name',countries_to_analyze)
+
+
+print(df_year_col_energy.describe())
+
+
+df_year_col_urban= filter_field_data(df_year_col,'Indicator Name',['Urban population (% of total population)'])
+df_year_col_urban  = filter_field_data(df_year_col_urban,'Country Name',countries_to_analyze)
+
+
+
+
+def plotting_time_series(data,indicator_label):
     '''
-    plot year wise data for respective countries
+    plot time series graph for indicators.
     '''
-    df = data.copy()
-    # Filter numeric columns for plotting
-    df.set_index('Country Name', inplace=True)
-    numeric_columns = df.columns[df.dtypes == 'float64']
-    df_numeric = df[numeric_columns]
+    df_sample = data.copy()
+    df_sample.set_index('Country Name', inplace=True)
+    numeric_columns_need = df_sample.columns[df_sample.dtypes == 'float64']
+    df_numeric_need = df_sample[numeric_columns_need]
 
-    # Plotting
     plt.figure(figsize=(12, 6))
-    for country in df_numeric.index:
-        plt.plot(
-            df_numeric.columns,
-            df_numeric.loc[country],
-            label=country, linestyle='dashed', marker='o')
+    for count in df_numeric_need.index:
+        plt.plot(df_numeric_need.columns, df_numeric_need.loc[count], label=count, linestyle='dashed', marker='o')
 
-    plt.title(indicator)
+    plt.title(indicator_label)
     plt.xlabel('Year')
-    #plt.ylabel('Urban Population (% of Total Population)')
-    plt.legend(title='Country', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.legend(title='Country', bbox_to_anchor=(1.15, 1), loc='upper left')
 
     plt.show()
 
 
-# Loading the dataset
-df_year_col,df_country_col = load_dataframe('world_bank_climate.xlsx')
 
-#taking data from 1990 to 2020 on 5 year gap.
-df_year_col_temp = years_to_analyze_data(df_year_col,1990,2021,5)
-
-countries = \
-    df_year_col_temp['Country Name'].value_counts().index.tolist()[0:15]
-
-# Looking at statistics of the data
-print(df_year_col_temp.describe())
-
-df_year_col_co2_emiss = data_specific_field(
-    df_year_col_temp,
-    'Indicator Name',
-    ['CO2 emissions (metric tons per capita)'])
-
-print(df_year_col_co2_emiss.describe())
-
-df_year_col_co2_emiss_country = data_specific_field(
-    df_year_col_co2_emiss,'Country Name',countries)
-
-print(df_year_col_co2_emiss_country.describe())
-
-bar_plot_country_year(
-    df_year_col_co2_emiss_country,
-    'CO2 emissions (metric tons per capita)')
-
-
-df_year_col_fresh_water = data_specific_field(
-    df_year_col_temp,
-    'Indicator Name',
-    ['Annual freshwater withdrawals, total (% of internal resources)'])
-df_year_col_fresh_water  = data_specific_field(
-    df_year_col_fresh_water,'Country Name',countries)
-
-print(df_year_col_fresh_water.describe())
-
-bar_plot_country_year(
-    df_year_col_fresh_water,
-    'Annual freshwater withdrawals, total (% of internal resources)')
-
-
-df_year_col_korea = data_specific_field(
-    df_year_col_temp,'Country Name',['Korea, Rep.'])
-
-data_heat_map_korea = get_data_indicator(df_year_col_korea)
-
-features_check = [
-    "CO2 emissions (metric tons per capita)",
-    "Annual freshwater withdrawals, total (% of internal resources)",
-    "Urban population (% of total population)",
-    "Total greenhouse gas emissions (kt of CO2 equivalent)",
-    "Agricultural land (% of land area)",
-    "Arable land (% of land area)",
-    "Renewable energy consumption (% of total final energy consumption)",
-    "Average precipitation in depth (mm per year)"]
-
-data_heat_map_korea_sub = data_heat_map_korea[features_check]
-
-print(data_heat_map_korea_sub.corr())
-
-sns.heatmap(
-    data_heat_map_korea_sub.corr(),
-    annot=True, cmap='YlGnBu', linewidths=.5, fmt='.3g')
+plotting_time_series(df_year_col_energy,'Energy use (kg of oil equivalent per capita)')
 
 
 
-df_year_col_ghg = data_specific_field(
-    df_year_col_temp,
-    'Indicator Name',
-    ['Total greenhouse gas emissions (kt of CO2 equivalent)'])
-df_year_col_ghg = data_specific_field(
-    df_year_col_ghg,'Country Name',countries)
+df_year_col_school= filter_field_data(df_year_col,'Indicator Name',['School enrollment, primary and secondary (gross), gender parity index (GPI)'])
+df_year_col_school  = filter_field_data(df_year_col_school,'Country Name',countries_to_analyze)
 
 
-print(df_year_col_ghg.describe())
+df_year_col_school.describe()
 
 
 
-df_year_col_ub_pop = data_specific_field(
-    df_year_col_temp,
-    'Indicator Name',
-    ['Urban population (% of total population)'])
-df_year_col_ub_pop = data_specific_field(
-    df_year_col_ub_pop,'Country Name',countries)
+plotting_time_series(df_year_col_school,'School enrollment, primary and secondary (gross), gender parity index (GPI)')
 
 
 
-time_series_plot(
-    df_year_col_ghg,'Total greenhouse gas emissions (kt of CO2 equivalent)')
+df_year_col_new = filter_field_data(df_year_col,'Country Name',['New Zealand'])
+data_heat_map_new = filter_indicator_data(df_year_col_new)
+data_heat_map_new_sub = data_heat_map_new[["Urban population growth (annual %)","Mortality rate, under-5 (per 1,000 live births)","CO2 intensity (kg per kg of oil equivalent energy use)","Energy use (kg of oil equivalent per capita)","Electricity production from coal sources (% of total)","School enrollment, primary and secondary (gross), gender parity index (GPI)"]]
+sns.heatmap(data_heat_map_new_sub.corr(), annot=True, cmap='YlGnBu', linewidths=.5, fmt='.3g')
 
 
 
-df_year_col_ag_land = data_specific_field(
-    df_year_col_temp,'Indicator Name',['Agricultural land (% of land area)'])
-df_year_col_ag_land = data_specific_field(
-    df_year_col_ag_land,'Country Name',countries)
-
-
-
-print(df_year_col_ag_land.describe())
-
-
-time_series_plot(df_year_col_ag_land,'Agricultural land (% of land area)')
-
-
-
-df_year_col_islam = data_specific_field(
-    df_year_col_temp,'Country Name',['Iran, Islamic Rep.'])
-data_heat_map_islam = get_data_indicator(df_year_col_islam)
-data_heat_map_islam_sub = data_heat_map_islam[
-    ["CO2 emissions (metric tons per capita)",
-     "Annual freshwater withdrawals, total (% of internal resources)",
-     "Urban population (% of total population)",
-     "Total greenhouse gas emissions (kt of CO2 equivalent)",
-     "Agricultural land (% of land area)",
-     "Arable land (% of land area)",
-     "Renewable energy consumption (% of total final energy consumption)",
-     "Average precipitation in depth (mm per year)"]]
-sns.heatmap(
-    data_heat_map_islam_sub.corr(),
-    annot=True, cmap='YlGnBu', linewidths=.5, fmt='.3g')
-
-
-
-df_year_col_uk = data_specific_field(
-    df_year_col_temp,'Country Name',['United Kingdom'])
-data_heat_map_uk = get_data_indicator(df_year_col_islam)
-data_heat_map_uk_sub = data_heat_map_uk[
-    ["CO2 emissions (metric tons per capita)",
-     "Annual freshwater withdrawals, total (% of internal resources)",
-     "Urban population (% of total population)",
-     "Total greenhouse gas emissions (kt of CO2 equivalent)",
-     "Agricultural land (% of land area)",
-     "Arable land (% of land area)",
-     "Renewable energy consumption (% of total final energy consumption)",
-     "Average precipitation in depth (mm per year)"]]
-sns.heatmap(data_heat_map_uk_sub.corr(), annot=True,linewidths=.5, fmt='.3g')
-
+df_year_col_swe= filter_field_data(df_year_col,'Country Name',['Sweden'])
+data_heat_map_swe = filter_indicator_data(df_year_col_swe)
+data_heat_map_swe_sub = data_heat_map_swe[["Urban population growth (annual %)","Mortality rate, under-5 (per 1,000 live births)","CO2 intensity (kg per kg of oil equivalent energy use)","Energy use (kg of oil equivalent per capita)","Electricity production from coal sources (% of total)","School enrollment, primary and secondary (gross), gender parity index (GPI)"]]
+sns.heatmap(data_heat_map_swe_sub.corr(), annot=True, cmap='YlGnBu', linewidths=.5, fmt='.3g')
 
 
 
